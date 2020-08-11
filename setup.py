79f1b368c17ae739a045bba478696f1a82e2545e
darshn <darshan.patel@arculus.de>
2020-08-11 18:24:16 +0200

    add setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="asyncpubsub",
+    version="0.0.1",
+    author="fr0zty",
+    author_email="darshn0108@gmail.com",
+    description="simple publisher-subscriber for python",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/fR0zTy/asyncpubsub.git",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU GPL3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
