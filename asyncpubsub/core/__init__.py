ca83efc46355a845585360b480d656e0c35d5b98
darshn <darshan.patel@arculus.de>
2020-08-15 11:05:55 +0200

    add __str__ and __repr__ to Registerable

--- asyncpubsub/core/__init__.py
+++ asyncpubsub/core/__init__.py
@@ -32,3 +32,9 @@ class Registerable:
 
     def __hash__(self):
         return hash((self.etype, self.name))
+
+    def __str__(self):
+        return f"{self.__class__.__name__}(name={self._name}, etype={self._etype})"
+
+    def __repr__(self):
+        return self.__str__()
