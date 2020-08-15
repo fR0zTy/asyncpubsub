b0171cff1a13b14fc84aaec76884260f8f7c6ba7
darshn <darshan.patel@arculus.de>
2020-08-15 12:11:56 +0200

    add skeleton for Subscriber

---
+++ asyncpubsub/core/subscriber.py
@@ -0,0 +1,12 @@
+# -*- coding : utf-8 -*-
+
+from asyncpubsub.core import EType, Registerable
+from asyncpubsub.core.hub import get_hub
+
+
+class Subscriber(Registerable):
+
+    def __init__(self, name):
+        super().__init__(name, EType.SUBSCRIBER)
+        self._hub = get_hub()
+        self._hub.register(self)
