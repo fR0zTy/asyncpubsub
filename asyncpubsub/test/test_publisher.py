e1b6a9439be3997fa39578ba3061dc7945cac417
darshn <darshan.patel@arculus.de>
2020-08-15 13:11:12 +0200

    fix name in test_publisher

--- asyncpubsub/test/test_publisher.py
+++ asyncpubsub/test/test_publisher.py
@@ -8,11 +8,11 @@ class TestPublisher(unittest.TestCase):
 
     def setUp(self):
         self.hub = get_hub()
-        self.publisher = Publisher('test_publisher')
+        self.publisher = Publisher('published_integers')
 
     def test_publisher_duplicate_name_raises_error(self):
         with self.assertRaises(RegistrationError):
-            Publisher('test_publisher')
+            Publisher('published_integers')
 
     def test_default_publisher_auto_registers(self):
         self.assertTrue(self.hub.is_registered(self.publisher))
