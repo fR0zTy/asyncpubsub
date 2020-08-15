e82c498a3f426c17d8787fc55e908c3ec2dbf3f8
darshn <darshan.patel@arculus.de>
2020-08-15 10:28:24 +0200

    add spin function to package

--- asyncpubsub/__init__.py
+++ asyncpubsub/__init__.py
@@ -1,8 +1,23 @@
 # -*- coding : utf-8 -*-
 
+import asyncio
 
-from asyncpubsub.core.hub import get_hub, Hub
+from asyncpubsub.core.hub import get_hub, Hub, RegistrationError, Registerable
 from asyncpubsub.core.publisher import Publisher
 
 
-__all__ = ["get_hub", "Hub", "Publisher"]
+def spin(loop=None):
+    if loop is None:
+        loop = asyncio.get_event_loop()
+    try:
+        loop.run_forever()
+    except Exception:
+        tasks = asyncio.all_tasks()
+        for task in tasks:
+            task.cancel()
+        loop.run_until_complete(tasks)
+    finally:
+        loop.close()
+
+
+__all__ = ["RegistrationError", "Registerable", "get_hub", "Hub", "Publisher", "spin"]
