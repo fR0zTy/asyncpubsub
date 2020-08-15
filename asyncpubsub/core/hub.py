76031c7cfd67ebedac2a83e904f6c434a667b9eb
darshn <darshan.patel@arculus.de>
2020-08-15 12:06:35 +0200

    add deregister and reset methods to Hub

--- asyncpubsub/core/hub.py
+++ asyncpubsub/core/hub.py
@@ -1,9 +1,9 @@
 # -*- coding : utf-8 -*-
 
 import logging
-from typing import Dict, Tuple, Set
+from typing import Set, Dict, Tuple
 
-from asyncpubsub.core import Registerable, EType
+from asyncpubsub.core import EType, Registerable
 
 _HUB = None
 
@@ -52,13 +52,14 @@ class Hub:
         if not isinstance(registerable, Registerable):
             raise TypeError("arg registerable must be of type Registerable")
 
-        key = (registerable.etype, registerable.name)
+        key = self.__gen_key(registerable)
+
         if key in self._registered:
             raise RegistrationError((f"{registerable.__class__.__name__} with name {registerable.name} "
                                      f" and etype {registerable.etype} already exists!"))
 
         self._registered[key] = registerable
-        self.logger.debug(f"Registered object of type {registerable.__class__.__name__} with key {key}")
+        self.logger.debug(f"registered object of type {registerable.__class__.__name__} with key {key}")
 
         if registerable.etype == EType.PUBLISHER:
             assert registerable not in self._publisher_subscriber_map
@@ -66,22 +67,58 @@ class Hub:
 
             # Handle all the dangling subscribers for this publisher
             added_subscribers = []
+
             for subscriber in self._dangling_subscribers:
                 if subscriber.name == registerable.name:
                     self._publisher_subscriber_map[registerable].add(subscriber)
                     added_subscribers.append(subscriber)
+                    self.logger.debug(f"added dangling subscriber {subscriber} for {registerable}")
+
             for subscriber in added_subscribers:
+                self.logger.debug(f"removed dangling subscriber {subscriber}, No longer dangling!")
                 self._dangling_subscribers.remove(subscriber)
 
         elif registerable.etype == EType.SUBSCRIBER:
             registered_publisher = self._registered.get((EType.PUBLISHER, registerable.name))
             if registered_publisher is not None:
                 self._publisher_subscriber_map[registered_publisher].add(registerable)
+                self.logger.debug(f'added subscriber {registerable} for {registered_publisher}')
             else:
                 self._dangling_subscribers.add(registerable)
+                self.logger.debug(f"added subscriber {registerable} into dangling subscribers")
 
-        else:
-            raise ValueError(f"Invalid registerable of type {registerable.__class__}")
+    def deregister(self, registerable):
+        """
+        Method for de-registration of a given registerable. This will de-register the object
+        and all the communication channels will be terminated
+        """
+        self._registered.pop(self.__gen_key(registerable), None)
+        self.logger.debug(f"deregistered {registerable}")
+
+        if registerable.etype == EType.PUBLISHER:
+            self._publisher_subscriber_map.pop(registerable, None)
+            self.logger.debug(f"removed publisher {registerable}")
+
+        elif registerable.etype == EType.SUBSCRIBER:
+            self._dangling_subscribers.discard(registerable)
+
+            found = False
+
+            for publisher, subscribers in self._publisher_subscriber_map.items():
+                for subscriber in subscribers:
+                    if subscriber is registerable:
+                        found = True
+                        break
+
+                if found:
+                    break
+
+            if found:
+                self._publisher_subscriber_map[publisher].remove(subscriber)
+                self.logger.debug(f"removed subscriber for {publisher}")
+
+    def __gen_key(self, registerable):
+        return (registerable.etype, registerable.name)
 
     def get_registered(self, etype=None):
         """
@@ -93,7 +130,7 @@ class Hub:
         if etype is None:
             return list(self._registered.values())
         else:
-            return [r for r in self._registered if r.etype == etype]
+            return [r for r in self._registered.values() if r.etype == etype]
 
     def is_registered(self, registerable):
         """
@@ -101,4 +138,16 @@ class Hub:
 
         :param Registerable registerable: instance to check for registration
         """
-        return bool(self._registered.get((registerable.etype, registerable.name)))
+        return bool(self._registered.get(self.__gen_key(registerable)))
+
+    def reset(self):
+        """
+        Method resets the hub's state to default state
+
+        .. warning:: Calling this will deregister all the registered entities and all communication
+                     channels will no longer be active. User caution is advised!
+        """
+        self._registered.clear()
+        self._publisher_subscriber_map.clear()
+        self._dangling_subscribers.clear()
+        self.logger.warning(f"{self.__class__.__name__} reset")
