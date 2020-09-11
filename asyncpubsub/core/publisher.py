4f06bc56bc37f88c09ecc9c61ac383512a8a3e5f
darshn <darshan.patel@arculus.de>
2020-09-11 16:54:16 +0200

    Publisher: add Subscribers property

--- asyncpubsub/core/publisher.py
+++ asyncpubsub/core/publisher.py
@@ -14,5 +14,10 @@ class Publisher(ChannelRegistrable):
         self._hub = get_hub()
         self._hub.register(self)
 
+    @property
+    def subscribers(self):
+        assert self._hub.is_mapped_channel_registrable(self), "This should never happen, how did this happen o_O ?"
+        return self._hub.get_registered(channels=[self.channel_name], etype=EType.SUBSCRIBER)
+
     def __del__(self):
         self._hub.deregister(self)
