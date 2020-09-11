2d203d8ce87dc058d7a2d15d493533677c15ebef
darshn <darshan.patel@arculus.de>
2020-09-11 14:10:27 +0200

    Subscriber: Remove leftover Registrabe referebces

--- asyncpubsub/core/subscriber.py
+++ asyncpubsub/core/subscriber.py
@@ -1,10 +1,10 @@
 # -*- coding : utf-8 -*-
 
-from asyncpubsub.core import EType, Registerable
+from asyncpubsub.core import EType, ChannelRegisterable
 from asyncpubsub.core.hub import get_hub
 
 
-class Subscriber(Registerable):
+class Subscriber(ChannelRegisterable):
 
     def __init__(self, name):
         super().__init__(name, EType.SUBSCRIBER)
