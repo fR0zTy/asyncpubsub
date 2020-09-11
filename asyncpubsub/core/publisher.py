66b6e91127be7ed00cdb3cb9f8daa09939be8058
darshn <darshan.patel@arculus.de>
2020-09-11 14:09:56 +0200

    Publisher: Remove leftover Registerable  references

--- asyncpubsub/core/publisher.py
+++ asyncpubsub/core/publisher.py
@@ -2,11 +2,11 @@
 
 import asyncio
 
-from asyncpubsub.core import EType, Registerable
+from asyncpubsub.core import EType, ChannelRegisterable
 from asyncpubsub.core.hub import get_hub
 
 
-class Publisher(Registerable):
+class Publisher(ChannelRegisterable):
 
     def __init__(self, name, queue_size=0):
         super().__init__(name, EType.PUBLISHER)
