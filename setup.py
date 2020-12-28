f7b7dd58cbc08e151b29a048df0340abe4adf96e
fr0zty <darshn0108@gmail.com>
2020-12-28 16:43:53 +0100

    Actions: remove compatibility for py.3.7

--- setup.py
+++ setup.py
@@ -18,5 +18,5 @@ setuptools.setup(
         "License :: OSI Approved :: GNU GPL3",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.7',
+    python_requires='>=3.8',
 )
