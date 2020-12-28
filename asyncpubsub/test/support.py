a0ca71922fcec4a309bab221d9c6efb14541d1e1
fr0zty <darshn0108@gmail.com>
2020-12-28 15:15:37 +0100

    Support: add callbacks to TrackedSubscriber

--- asyncpubsub/test/support.py
+++ asyncpubsub/test/support.py
@@ -28,16 +28,28 @@ class TrackedSubscriber(Subscriber):
 
     def __init__(self, channel_name, callback, queue_size=0):
         super().__init__(channel_name, callback, queue_size=queue_size)
+        self._notified_messages = []
         self._received_messages = []
 
     @property
-    def received_messages(self):
-        return self._received_messages
+    def notified_messages(self):
+        return self._notified_messages
 
     def notify(self, msg):
         super().notify(msg)
-        self._received_messages.append(msg)
+        self._notified_messages.append(msg)
 
     async def wait_for_queue_empty(self):
         while not self._msg_queue.empty():
+            await asyncio.sleep(0.1)
+
+    def sync_callback(self, msg):
+        self._received_messages.append(msg)
+
+    async def async_callback(self, msg):
+        await asyncio.sleep(0.1)
+        self._received_messages.append(msg)
+
+    async def wait_for_n_messages(self, n):
+        while not len(self._received_messages) == n:
             await asyncio.sleep(0.1)
