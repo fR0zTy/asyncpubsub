91ee9a7c6bcbe6981200b01e87bfbf319618f8c9
fr0zty <darshn0108@gmail.com>
2020-12-28 14:06:07 +0100

    Tests: add publish message tests

--- asyncpubsub/test/test_publisher.py
+++ asyncpubsub/test/test_publisher.py
@@ -1,15 +1,17 @@
 # -*- coding : utf-8 -*-
 
+import random
 import unittest
 
 from asyncpubsub import Publisher, Subscriber, RegistrationError, get_hub
+from asyncpubsub.test.support import TrackedPublisher
 
 
-class TestPublisher(unittest.TestCase):
+class TestPublisher(unittest.IsolatedAsyncioTestCase):
 
     def setUp(self):
         self.hub = get_hub()
-        self.publisher = Publisher('int-channel')
+        self.publisher = TrackedPublisher('int-channel')
 
     def test_publisher_duplicate_name_raises_error(self):
         with self.assertRaises(RegistrationError):
@@ -33,6 +35,20 @@ class TestPublisher(unittest.TestCase):
         subscriber_3 = Subscriber('float-channel', lambda: None)
         publisher_subscribers = self.publisher.subscribers
         self.assertFalse(subscriber_3 in publisher_subscribers)
+
+    def test_published_no_wait(self):
+        messages = [random.randint(0, 100) for i in range(random.randint(5, 10))]
+        for msg in messages:
+            self.publisher.publish_nowait(msg)
+
+        self.assertTrue(all(i == j for i, j in zip(messages, self.publisher.published_messages)))
+
+    async def test_published_default(self):
+        messages = [random.randint(0, 100) for i in range(random.randint(5, 10))]
+        for msg in messages:
+            await self.publisher.publish(msg)
+
+        self.assertTrue(all(i == j for i, j in zip(messages, self.publisher.published_messages)))
 
     def tearDown(self):
         self.hub.reset()
