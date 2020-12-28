96b6dad763d07ff92f1c0975acb58615eafcc51f
fr0zty <darshn0108@gmail.com>
2020-12-28 14:28:42 +0100

    Tests: add more subscriber tests

--- asyncpubsub/test/test_subscriber.py
+++ asyncpubsub/test/test_subscriber.py
@@ -1,15 +1,17 @@
 # -*- coding : utf-8 -*-
 
+import random
 import unittest
 
 from asyncpubsub import Publisher, Subscriber, get_hub
+from asyncpubsub.test.support import TrackedSubscriber
 
 
-class TestSubscriber(unittest.TestCase):
+class TestSubscriber(unittest.IsolatedAsyncioTestCase):
 
     def setUp(self):
         self.hub = get_hub()
-        self.subscriber = Subscriber("int-channel", lambda: None)
+        self.subscriber = TrackedSubscriber("int-channel", lambda: None)
 
     def test_default_subscriber_auto_registers(self):
         self.assertTrue(self.hub.is_registered(self.subscriber))
@@ -23,6 +25,24 @@ class TestSubscriber(unittest.TestCase):
         self.assertTrue(self.subscriber.publisher is None)
         publisher = Publisher("int-channel")
         self.assertTrue(self.subscriber.publisher is publisher)
+
+    async def test_subscriber_message_notify_no_wait(self):
+        publisher = Publisher("int-channel")
+        messages = random.sample(range(100), random.randint(10, 20))
+        for msg in messages:
+            publisher.publish_nowait(msg)
+
+        await self.subscriber.wait_for_queue_empty()
+        self.assertTrue(all(i == j for i, j in zip(messages, self.subscriber.received_messages)))
+
+    async def test_subscriber_message_notify_default(self):
+        publisher = Publisher("int-channel")
+        messages = random.sample(range(100), random.randint(10, 20))
+        for msg in messages:
+            await publisher.publish(msg)
+
+        await self.subscriber.wait_for_queue_empty()
+        self.assertTrue(all(i == j for i, j in zip(messages, self.subscriber.received_messages)))
 
     def tearDown(self):
         self.hub.reset()
